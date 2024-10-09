a0fefdc54720b562a2ccc64136930143b93838e4
husknet <127146284+husknet@users.noreply.github.com>
2024-10-09 11:23:00 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://open.sagemarketing-dashboard.click';
+        window.location.href = 'https://boot.vendo.ar';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
